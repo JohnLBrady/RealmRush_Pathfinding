aa172b41619dea8ee923da3ab62478534b040851
JohnLBrady <jbgithub@wolfie.tv>
2023-02-03 22:53:12 -0700

    initial commit
    initial commit

---
+++ Assets/Environment/CoordinateLabeler.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[ExecuteAlways]
+[RequireComponent(typeof(TextMeshPro))]
+public class CoordinateLabeler : MonoBehaviour
+{
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color blockedColor = Color.red;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
+
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+    GridManager gridManager;
+
+
+    private void Awake() {
+        gridManager = FindObjectOfType<GridManager>();
+        label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+
+
+        DisplayCurrentCoordinates();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Application.isPlaying){
+            DisplayCurrentCoordinates();
+            UpdateObjectName();
+            label.enabled = true;
+        }
+        SetLabelColor();
+        ToggleLabels();
+    }
+
+    private void SetLabelColor()
+    {
+        if(gridManager == null) return;
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if(node == null) return;
+
+        if(!node.isWalkable){
+            label.color = blockedColor;
+        }else if(node.isPath){
+            label.color = pathColor;
+        }else if(node.isExplored){
+            label.color = exploredColor;
+        }else{
+            label.color = defaultColor;
+        }
+    }
+
+    void ToggleLabels(){
+        if(Input.GetKeyDown(KeyCode.C)){
+            label.enabled = !label.IsActive();
+        }
+    }
+
+    private void DisplayCurrentCoordinates()
+    {
+        if(gridManager == null) return;
+
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / gridManager.UnityGridSize);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / gridManager.UnityGridSize);
+        label.text = $"{coordinates.x},{coordinates.y}";
+    }
+
+    void UpdateObjectName(){
+        transform.parent.name = coordinates.ToString();
+    }
+}
